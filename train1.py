6f4664ca44dc1ff5d70a2f440cfe77bda23185e2
shota0121-spec <shota0121@g.ecc.u-tokyo.ac.jp>
2026-07-09 09:41:17 +0000

    train変更

--- train1.py
+++ train1.py
@@ -2,14 +2,14 @@
 # 学習したモデルを models/models1/ に出力するように．
 import os
 import sys
-import my_library.trainer as trainer
-preprocessed_train_data_path=input("Enterthepathforthepreprocesseddata:")
-model_dump_path_base=input("Enterthepathforthedirectorytosavethemodels:")
+import src.my_library.trainer1 as trainer
+preprocessed_train_data_path=input("Enter the path for the preprocessed data:")
+model_dump_path_base=input("Enter the path for the directory to save the models:")
 t=trainer.Trainer()
 X,y=t.load_data(preprocessed_train_data_path)
 hyperparameters=[0.1,1,10,100,1000]
 for h in hyperparameters:
     model_dump_path=os.path.join(model_dump_path_base,"SVM_"+str(h)+".pkl")
     print(model_dump_path)
-    t.train(h,X,y)
-    t.dump_models(model_dump_path)
+    t.train_SVM(h,X,y)
+    t.dump_model(model_dump_path)
