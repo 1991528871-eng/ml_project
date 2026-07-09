9378206a79593b86cfb136ba4d7256bb30495740
shota0121-spec <shota0121@g.ecc.u-tokyo.ac.jp>
2026-07-09 08:48:06 +0000

    trainerを変更

--- train1.py
+++ train1.py
@@ -1,2 +1,15 @@
 # 柚木翔太・J4260079  学習フェーズ 1
 # 学習したモデルを models/models1/ に出力するように．
+import os
+import sys
+import my_library.trainer as trainer
+preprocessed_train_data_path=input("Enterthepathforthepreprocesseddata:")
+model_dump_path_base=input("Enterthepathforthedirectorytosavethemodels:")
+t=trainer.Trainer()
+X,y=t.load_data(preprocessed_train_data_path)
+hyperparameters=[0.1,1,10,100,1000]
+for h in hyperparameters:
+    model_dump_path=os.path.join(model_dump_path_base,"SVM_"+str(h)+".pkl")
+    print(model_dump_path)
+    t.train(h,X,y)
+    t.dump_models(model_dump_path)
